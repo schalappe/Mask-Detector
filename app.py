8fe59e84bc71c1325abbbb09dfc84f6316004fbc
schalappe <konehakim@gmail.com>
2021-05-07 19:47:04 +0200

    first refactoring ...

--- app.py
+++ app.py
@@ -10,31 +10,31 @@ from PIL import Image
 from utils import visualize_detections
 
 # header
-st.title('COVID-19: Détection de masque')
+st.title("COVID-19: Détection de masque")
 # sidebar
-pages = ['Accueil', 'Essayer']
-choice = st.sidebar.selectbox('Menu', pages)
+pages = ["Accueil", "Essayer"]
+choice = st.sidebar.selectbox("Menu", pages)
 
 
 # load model
 @st.cache
 def load_data():
-    return [tf.keras.models.load_model('./model/best_nasnet.h5'), mtcnn.MTCNN()]
+    return [tf.keras.models.load_model("./model/best_nasnet.h5"), mtcnn.MTCNN()]
 
 
 # mask detection
 def mask_recognition(image):
     preds = []
-    img = np.array(image.convert('RGB'))
+    img = np.array(image.convert("RGB"))
     # detect and encode faces
     boxes = detector.detect_faces(img)
     for box in boxes:
-        x, y, width, height = box['box']
-        if width >= 30 and height >= 30 and box['confidence'] > 0.97:
-            face = img[y:y+height, x:x+width]
+        x, y, width, height = box["box"]
+        if width >= 30 and height >= 30 and box["confidence"] > 0.97:
+            face = img[y : y + height, x : x + width]
             face = Image.fromarray(face)
             face = face.resize((224, 224))
-            face = tf.keras.preprocessing.image.img_to_array(face)/255.0
+            face = tf.keras.preprocessing.image.img_to_array(face) / 255.0
             face = np.expand_dims(face, axis=0)
             # prediction
             preds.append(model.predict(face)[0][0])
@@ -46,73 +46,76 @@ def mask_recognition(image):
 def about():
     # description
     st.write(
-        'Cette application est un détecteur de masque dans une image. Dans un'
-        'contexte de COVID-19 où le port du masque est vivement recommander. '
+        "Cette application est un détecteur de masque dans une image. Dans un"
+        "contexte de COVID-19 où le port du masque est vivement recommander. "
         "Ceci démontre l'utilisation du deep learning pour détecter des masques"
-        'dans une image.'
+        "dans une image."
     )
-    st.write(
-        "Vous pouvez trouver le code sur [GitHub](https://github.com/schalappe/Mask-Detector)."
-    )
+    st.write("Vous pouvez trouver le code sur [GitHub](https://github.com/schalappe/Mask-Detector).")
     # function
-    st.header('Fonctionnement')
+    st.header("Fonctionnement")
     st.write(
-        '''
-            Cette application a été bâtie en deux étapes: 
+        """
+            Cette application a été bâtie en deux étapes:
             \n1. Présence d'un masque dans l'image: Oui ou Non ?
             \n2. Détecter le masque dans l'image.
-        '''
+        """
     )
     # step 1
-    st.subheader('Etape 1: Présence')
+    st.subheader("Etape 1: Présence")
     st.write(
-        'Dans la première étape, il faut bâtir un modèle de classification '
-        'pour savoir si oui ou non une image contient un masque. '
-        'Voir le code [ici](https://github.com/schalappe/Mask-Challenge)'
+        "Dans la première étape, il faut bâtir un modèle de classification "
+        "pour savoir si oui ou non une image contient un masque. "
+        "Voir le code [ici](https://github.com/schalappe/Mask-Challenge)"
     )
     # step 2
-    st.subheader('Etape 2: Détection')
+    st.subheader("Etape 2: Détection")
     st.write(
-        'Avec un modèle permettant de dire si une image contient un masque. '
-        'On peut bâtir un application pour détecter des masques sur les visages '
-        'des personne. Cette application fonction en 3 phases:\n'
+        "Avec un modèle permettant de dire si une image contient un masque. "
+        "On peut bâtir un application pour détecter des masques sur les visages "
+        "des personne. Cette application fonction en 3 phases:\n"
     )
     st.write(
-        '''
+        """
             1. utiliser un modèle pour détecter des visages
             2. extraire les visages détectés
             3. determiner si l'image (visage) contient un masque
-        '''
+        """
     )
     st.write(
-        'Pour la détection de visage, on utilise la bibliothèque: '
-        '[MTCNN](https://github.com/ipazc/mtcnn). '
+        "Pour la détection de visage, on utilise la bibliothèque: "
+        "[MTCNN](https://github.com/ipazc/mtcnn). "
         "Ce qui peut poser certains problème. Car le succès de l'application "
         "dépend dès lors du bon fonctionnement de ce package."
     )
     # examples
-    st.header('Exemples')
-    st.image('./img/5.jpg', width=708, caption='Exemple 1: Masque détecté')
-    st.image('./img/3.jpeg', width=708, caption='Exemple 2: Sans masque')
+    st.header("Exemples")
+    st.image("./img/5.jpg", width=708, caption="Exemple 1: Masque détecté")
+    st.image("./img/3.jpeg", width=708, caption="Exemple 2: Sans masque")
     # go further
-    st.header('Aller plus loin')
-    st.write("L'utilisation de l'application de detection de visage peut engendrer des erreurs. "
-             "Si le visage n'est pas reconnu, il n'y a pas de détection de "
-             "masque. Pour parler aller, on peut entraîner spécifiquement un "
-             "modèle pour détecter les masques.")
-    st.image('./img/0.jpg', width=708,
-             caption="Exemple 3: Visage non détecté par le package bien que le "
-                     "modèle est détecté la présence d'un masque")
+    st.header("Aller plus loin")
+    st.write(
+        "L'utilisation de l'application de detection de visage peut engendrer des erreurs. "
+        "Si le visage n'est pas reconnu, il n'y a pas de détection de "
+        "masque. Pour parler aller, on peut entraîner spécifiquement un "
+        "modèle pour détecter les masques."
+    )
+    st.image(
+        "./img/0.jpg",
+        width=708,
+        caption="Exemple 3: Visage non détecté par le package bien que le "
+        "modèle est détecté la présence d'un masque",
+    )
 
 
 # main
 def main():
 
-    if choice == 'Accueil':
+    if choice == "Accueil":
         about()
-    elif choice == 'Essayer':
+    elif choice == "Essayer":
         st.write("Uploder une image avec des visages pour tester l'application.")
-        file = st.file_uploader(" ", type=['jpeg', 'png', 'jpg'])
+        file = st.file_uploader(" ", type=["jpeg", "png", "jpg"])
         temp_file = NamedTemporaryFile(delete=False)
         if file is not None:
             temp_file.write(file.getvalue())
@@ -121,11 +124,12 @@ def main():
                 try:
                     result_img = mask_recognition(image)
                     st.image(result_img, use_column_width=True)
-                except:
-                    st.write('Nous avons rencontré certains problèmes avec cette image ...')
+                except Exception as e:
+                    print(e)
+                    st.write("Nous avons rencontré certains problèmes avec cette image ...")
                     st.image(image, use_column_width=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     model, detector = load_data()
     main()
--- utils/viz.py
+++ utils/viz.py
@@ -5,23 +5,19 @@ import cv2
 def visualize_detections(image, boxes, preds):
     for (box, pred) in zip(boxes, preds):
         # unpack the bounding box
-        x, y, width, height = box['box']
+        x, y, width, height = box["box"]
         # determine class
         label = "Masque" if pred > 0.5 else "Sans masque"
         color = (0, 255, 0) if label == "Masque" else (255, 0, 0)
         # include the probability in the label
-        cv2.rectangle(image, (x, y+height - 35), (x+width, y+height), color, cv2.FILLED)
-        cv2.putText(
-            image, label, (x + 6, y+height - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 0, 0), 1
-        )
-        cv2.rectangle(image, (x, y), (x+width, y+height), color, 2)
+        cv2.rectangle(image, (x, y + height - 35), (x + width, y + height), color, cv2.FILLED)
+        cv2.putText(image, label, (x + 6, y + height - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 0, 0), 1)
+        cv2.rectangle(image, (x, y), (x + width, y + height), color, 2)
     return image
 
 
 def visualize_mask(image, pred):
     label = "Masque" if pred > 0.5 else "Sans masque"
     color = (0, 255, 0) if label == "Masque" else (0, 0, 255)
-    cv2.putText(
-        image, label, (20, 20), cv2.FONT_HERSHEY_DUPLEX, 1.0, color, 2
-    )
+    cv2.putText(image, label, (20, 20), cv2.FONT_HERSHEY_DUPLEX, 1.0, color, 2)
     return image
